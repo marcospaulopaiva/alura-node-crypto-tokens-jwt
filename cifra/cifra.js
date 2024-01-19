f27c8ad535c9c80d92c9991eb5ea12a9468c8789
marcospaulopaiva <marcos.poo@gmail.com>
2024-01-19 14:49:23 -0300

    Aula 1 - Conhecendo problema.

---
+++ cifra/cifra.js
@@ -0,0 +1,27 @@
+const mensagemSecreta = 'alura'
+
+console.log(mensagemSecreta);
+
+function cifraMensagem(mensagem, movimentos){
+    const mensagemCifrada = mensagem.split('').map( caractere => {
+        const codigoCaractere = caractere.charCodeAt(0)
+        return String.fromCharCode(codigoCaractere + movimentos)
+    })
+    return mensagemCifrada.join('') 
+}
+
+const mensagemCifrada = cifraMensagem(mensagemSecreta,4)
+console.log(mensagemCifrada)
+
+function decifraMensagem(mensagem, movimentos){
+    const mensagemCifrada = mensagem.split('').map( caractere => {
+        const codigoCaractere = caractere.charCodeAt(0)
+        return String.fromCharCode(codigoCaractere - movimentos)
+    })
+    return mensagemCifrada.join('') 
+}
+
+const mensagemDecifrada = decifraMensagem(mensagemCifrada,4)
+console.log(mensagemDecifrada)
+
+
